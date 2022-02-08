05e9ed9668d266b079af93f33865a43c467f6230
Gali Ravi Praveen <grpnpraveen@gmail.com>
2022-02-08 13:54:53 +0530

    till 3 divisors

--- Basic/isPrime.cpp
+++ Basic/isPrime.cpp
@@ -28,11 +28,28 @@
 
 using namespace std;
 
-
+      int isPrime(int n){
+    if(n==1)
+     return 0;
+    if(n==2||n==3) 
+    return 1;
+    if(n%2==0 || n%3==0)
+    return 0;
+        for(int i=5;i*i<= n;i=i+6)
+        {
+            if(n%i == 0)
+                return 0;
+            if(n%(i+2) == 0)
+                return 0;
+        }
+        return 1;
+            
+    }
 int main(int argc,char** argv[])
 {
 
-
+     cout<<isPrime(15);
+    
     cout<<"\n";
     return 0 ;
 }
---
+++ Basic/numbersWith3divisors.cpp
@@ -0,0 +1,82 @@
+#define _USE_MATH_DEFINES 
+#include<iostream>
+#include <algorithm>
+#include<sstream>
+#include<iomanip>
+#include<cmath>
+#include<string>   // it has getline(cin,var)
+#include<sstream>
+//stl containers
+#include<array>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<list>
+#include<forward_list>
+// #include<thread>
+// #include<chrono>
+#include<any>
+#define to_Str(X) #X
+#define glue(a,b) a##b  // 
+// #include<bits/stdc++.h>    # not working in msvc
+
+
+using namespace std;
+int m=1000001;
+void seive(vector<long long>&isprime,vector<long long>&primes){
+     
+        isprime[0] = 0;
+        isprime[1] = 0;
+        
+        for(int i = 2; i <= sqrt(m); i++){
+            
+            if(isprime[i]){
+                for(int j = 2; j*i <= m; j++){
+                    isprime[j*i] = 0;
+                }
+            }
+        }
+        int count = 0;
+        for(int i = 2; i <= m; i++){
+            
+            if(isprime[i])
+            count++;
+            
+            primes[i] = count;
+        }
+    }
+    
+    vector<int> threeDivisors(vector<long long> query, int q)
+    {
+        vector<long long>isprime(1000001,1);
+        vector<long long>primes(1000001,0);
+
+        seive(isprime,primes);
+
+        vector<int>v;
+        for(int i = 0; i < q; i++)
+        {
+            int p = primes[int(sqrt(query[i]))];
+            v.push_back(p);
+        }
+        
+        return v;
+    }
+int main(int argc,char** argv[])
+{
+     int q=1;
+      vector<long long> query;
+      query.push_back(1000000000000);
+        vector<int> R= threeDivisors(query,1);
+        for(auto x: R)
+        {
+            cout<<x<<" ";
+        }
+    cout<<"\n";
+    return 0 ;
+}
