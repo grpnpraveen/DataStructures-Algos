c7c9e397e700ede265d06b66acc6400693a569ac
Gali Ravi Praveen <grpnpraveen@gmail.com>
2022-02-08 08:10:32 +0530

    ds

--- test.cpp
+++ test.cpp
@@ -1,11 +1,41 @@
 #include<iostream>
+#include <algorithm>
+#include<sstream>
+#include<iomanip>
+#include<math.h>
+#include<string>   // it has getline(cin,var)
+#include<sstream>
+//
+#include<array>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<map>
+#include<set>
+#include<list>
+// #include<thread>
+// #include<chrono>
+#include<any>
+#define to_Str(X) #X
+#define glue(a,b) a##b  // 
+// #include<bits/stdc++.h>    # not working in msvc
+
 
 using namespace std;
 
 
 int main()
 {
+    
+    cin.tie(NULL);
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        // logic
+        cout<<"HI";
 
+    };
 
 
     return 0 ;
