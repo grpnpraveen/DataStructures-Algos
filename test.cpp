b0a6ecd73d73eee1490e6b2dbf0d13760b04e45f
Gali Ravi Praveen <grpnpraveen@gmail.com>
2022-02-09 09:30:56 +0530

    Done

---
+++ test.cpp
@@ -0,0 +1,39 @@
+#define _USE_MATH_DEFINES 
+#include<iostream>
+#include <algorithm>
+#include<sstream>
+#include<iomanip>
+#include<cmath>
+#include<string>   // it has getline(cin,var)
+#include<sstream>
+//stl containers
+#include<array>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<list>
+#include<forward_list>
+// #include<thread>
+// #include<chrono>
+#include<any>
+#define to_Str(X) #X
+#define glue(a,b) a##b  // 
+// #include<bits/stdc++.h>    # not working in msvc
+
+
+using namespace std;
+
+
+int main(int argc,char** argv[])
+{
+
+   int p=1000000000;
+    cout<<p;
+    cout<<"\n";
+    return 0 ;
+}
